02ffb460d75d1e4fd1360f9e5efa45f51f7a88ac
Woogon Shim <woogon.shim@lge.com>
2013-11-27 16:32:20 +0900

    Rename filename

--- list.changed.block.from.file.sh
+++ list.changed.block.from.file.sh
@@ -13,7 +13,7 @@
 
 # 한달 동안 수정한 함수 목록과 수정 횟수 가져오기
 # git log -p --since="one month ago" -- $1 | grep -E '^(@@)' | sed 's/@@.*@@\s*//' | grep -E '[^\s*:]$' | grep -oE ' (\w+)\(' | grep -oE '(\w+)' | sort | uniq -c | sort -rn
-git log -p --since="one month ago" -- src/pbs_interface_json_parser.c | grep -E '^(@@)' | sed 's/@@.*@@\s*//' | grep -E '[^\s*:]$' | sort | uniq -c | sort -rn
+git log -p --since="one month ago" -- $1 | grep -E '^(@@)' | sed 's/@@.*@@\s*//' | grep -E '[^\s*:]$' | sort | uniq -c | sort -rn
 # https://coderwall.com/p/v1r8jq
 # 한달 동안 가장 커밋을 많이 한 사람
 # git log --pretty=format:%an --since="one month ago" | sort | uniq -c | sort -n | tac | head -30
