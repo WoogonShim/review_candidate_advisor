3829f5ccbdfc61e1c79612dba5c0aad4b9327b3d
Woogon Shim <woogon.shim@lge.com>
2013-11-27 16:32:20 +0900

    Rename filename

--- run.batch.at.sh
+++ run.batch.at.sh
@@ -7,8 +7,8 @@ echo -e "${HIGHLIGHT}===========================================================
 echo -e "${HIGHLIGHT} Doing batch analysis for all sub git repositories${NORMAL}"
 echo -e "${HIGHLIGHT}   > at '$1' ${NORMAL}"
 echo -e "${HIGHLIGHT}================================================================================${NORMAL}"
-#./listing.all.repos.from.sh $1
+#./list.all.repos.from.sh $1
 ./scan.all.repos.at.pl $1
-./gathering.all.repos.data.from.sh
+./gather.all.repos.data.from.sh
 ./top-risk-list.pl $1 $2 $3
 echo -e "${HIGHLIGHT} Batch analysis completed${NORMAL}"
